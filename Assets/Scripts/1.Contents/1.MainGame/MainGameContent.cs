d8acc2e6d3b157000548a9d44545198917c39679
JoDalung <whekfdnd30@naver.com>
2024-06-04 13:55:02 +0900

    06.04
    게임 로직 추가

--- Assets/Scripts/1.Contents/1.MainGame/MainGameContent.cs
+++ Assets/Scripts/1.Contents/1.MainGame/MainGameContent.cs
@@ -2,15 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Tile_type
+{
+   Fire,
+   Delay,
+   none,
+}
+    
+public class Tiles
+{
+    public Tile_type Right;
+    public Tile_type Left;    
+    
+    public Tiles(Tile_type right, Tile_type left)
+    {
+        Right = right;
+        Left  =  left;
+    }
+}
+
 public class MainGameContent : IContent
 {
     public IDialogLoader dialogLoader;
 
+    bool _StartGame = false;   
+    public List<Tiles> _TilesList = new List<Tiles>();
+
+
     protected override void _OnLoad()
     {
         dialogLoader.LoadDialog();
-    }
+        _StartGame = false;
+        //Set TileList
+        _SetTileList();
 
+    }
+    #region Framework
     protected override void _OnLoadComplete()
     {
         dialogLoader.ShowIdxDialog(); 
@@ -26,8 +53,37 @@ public class MainGameContent : IContent
         _RemoveEvent();
     }
 
+    void _AddEvent()
+    {
+        MainGameDialog.StartGame_act += MainGameDialog_StartGame_act;
+    
+    }
 
-    void _AddEvent(){}
-    void _RemoveEvent(){}
+    void _RemoveEvent()
+    {
+        MainGameDialog.StartGame_act -= MainGameDialog_StartGame_act;
+    }
+    #endregion
+
+    private void MainGameDialog_StartGame_act()
+    {
+        _StartGame = true;
+    }
+
+    void _SetTileList()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            int RandomNum = Random.Range(0, 2);
+            Tile_type Right;
+            Tile_type Left;
+
+            Right = RandomNum == 0 ? Tile_type.Fire : Tile_type.Delay;
+            Left = RandomNum == 1 ? Tile_type.Fire : Tile_type.Delay;
+            _TilesList.Add(new Tiles(Right, Left));
+            Debug.Log($"{i} : left = {Left} / right = {Right}");
+        }
+
+    }
 }
 
--- Assets/Scripts/1.Contents/1.MainGame/UI/MainGameDialog.cs
+++ Assets/Scripts/1.Contents/1.MainGame/UI/MainGameDialog.cs
@@ -5,13 +5,45 @@ using UnityEngine.UI;
 
 public class MainGameDialog : IDialog
 {
+    public static event System.Action StartGame_act;
+    public Sprite[] Tile_sp;
+    
+    MainGameContent _MainGameContent;
+    Text Start_txt;
+    RectTransform Tile;
+    RectTransform[] Tiles = new RectTransform[6];
+
+
+    #region Framework
     protected override void _OnLoad()
     {
         // ui caching
+        Debug.Log("MainGameDialog _OnLoad");
+        _MainGameContent = GameObject.Find("MainGameContent").GetComponent<MainGameContent>();
+        Start_txt = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>();
+
+        Tile = transform.GetChild(2).GetChild(0).GetChild(1).GetComponent<RectTransform>();
+
+        for (int i = 0; i < Tiles.Length; i++)
+        {
+            Tiles[i] = Tile.GetChild(i).GetComponent<RectTransform>();
+        }
+        
     }
     protected override void _OnLoadComplete()
     {
         // ui caching Complete
+        Debug.Log("MainGameDialog _OnLoadComplete");
+        Start_txt.gameObject.SetActive(false);
+        Tile.gameObject.SetActive(false);
+        for (int i = 0; i < Tiles.Length; i++)
+        {
+            Tiles[i].GetChild(0).GetChild(0).GetComponent<Image>().sprite = Tile_sp[(int)_MainGameContent._TilesList[i].Right];
+            Tiles[i].GetChild(1).GetChild(0).GetComponent<Image>().sprite = Tile_sp[(int)_MainGameContent._TilesList[i].Left];
+        }
+
+        StartCoroutine(_cStartGameTimer());
+
     }
 
     protected override void _OnEnter()
@@ -31,5 +63,25 @@ public class MainGameDialog : IDialog
     {
 
     }
+    #endregion
+
+    IEnumerator _cStartGameTimer()
+    {
+        Start_txt.gameObject.SetActive(true);
+        Start_txt.text = $"3";
+        yield return new WaitForSeconds(1.0f);
+        Start_txt.text = $"2";
+        yield return new WaitForSeconds(1.0f);
+        Start_txt.text = $"1";
+        yield return new WaitForSeconds(1.0f);
+        Start_txt.text = $"Start!";
+        yield return new WaitForSeconds(0.25f);
+        Start_txt.gameObject.SetActive(false);
+        Tile.gameObject.SetActive(true);
+        StartGame_act?.Invoke();
+    }
+
+
+
 }
 
